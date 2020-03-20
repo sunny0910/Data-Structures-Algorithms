852ba8196a07db8e693b44265f81fcac9d9458ac
Sunny Ramrakhiani <sunnyramrakhiani10@gmail.com>
2020-03-20 16:07:17 +0530

    Block doc

--- array/number_of_steps.py
+++ array/number_of_steps.py
@@ -1,7 +1,7 @@
 def number_of_ways(n):
     """
     Function to get the number of ways one can reach n steps if only 1/2/3 steps are allowed at a time.
-    This works like fibonaci series where we store answers of initial values and use them to calculate answers of above
+    This works like fibonacci series where we store answers of initial values and use them to calculate answers of above
     values.
     The number of ways to reach n steps is equal to sum of ways to reach (n-1), (n-2), and (n-3) steps
     :param n: Int
